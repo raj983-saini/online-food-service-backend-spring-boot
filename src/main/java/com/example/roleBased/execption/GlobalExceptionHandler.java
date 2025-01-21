c94f057f56abc63f503100fe50cd9403d5e1c911
raj983-saini <raj983saini@gmial.com>
2025-01-21 19:37:20 +0530

    complete bvackend   for online food service  project

--- src/main/java/com/example/roleBased/execption/GlobalExceptionHandler.java
+++ src/main/java/com/example/roleBased/execption/GlobalExceptionHandler.java
@@ -3,11 +3,11 @@ package com.example.roleBased.execption;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
